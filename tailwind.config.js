ef5c2f20f4595731ab793be8f5a911573aa7d410
Gagandeep Singh <gagansday@gmail.com>
2020-12-28 18:48:01 +0530

    init tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    colors: {
+      black: '#000000',
+      white: '#FFFFFF',
+      'dark-pink': '#751A3D',
+      pink: '#D61F69',
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
