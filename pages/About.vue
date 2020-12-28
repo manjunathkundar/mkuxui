fe019a74d8ebb15090ff72fbb514eae145cefdd0
Gagandeep Singh <gagansday@gmail.com>
2020-12-28 20:19:47 +0530

    add extra pages

---
+++ pages/About.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto">About</div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
---
+++ pages/Contact.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto">Contact</div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
