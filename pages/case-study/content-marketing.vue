5aa5dd6b02e2f502a878b24954ea812fd8d0f45a
Gagandeep Singh <gagansday@gmail.com>
2020-12-30 19:00:22 +0530

    overview section

--- pages/case-study/content-marketing.vue
+++ pages/case-study/content-marketing.vue
@@ -16,7 +16,53 @@
         class="h-full mx-auto mt-8 overflow-hidden"
       />
     </section>
-    <div class="bg-white"></div>
+    <div class="bg-white">
+      <section
+        class="container pt-8 mx-auto md:pt-16 md:grid md:gap-16 lg:gap-20 md:grid-cols-2"
+      >
+        <div>
+          <h4 class="text-2xl font-bold uppercase">OVERVIEW</h4>
+          <div class="text-base text-dark-gray">
+            <p class="mt-4">
+              My most recent work with
+              <a href="#" class="font-bold text-pink">Emerhub</a> where I
+              designed for some of its core offerings like Incorporation,
+              Payroll, Registry with a global reach that helped integrate all
+              their needs under one roof. Having led the module phase for one
+              years in this young,
+            </p>
+            <p class="mt-4">
+              <strong
+                >rapidly growing enterprise startup environment - taught me how
+                to balance business goals and engineering constraints as I
+                unrelentingly advocated for the user..</strong
+              >
+            </p>
+          </div>
+        </div>
+        <div class="mt-8 md:mt-auto">
+          <h4 class="text-2xl font-bold uppercase">ROLE & DURATION</h4>
+          <div class="px-2 mt-4">
+            <h6 class="text-base font-bold">Sr. Product Designer</h6>
+            <p class="mt-2 text-sm text-dark-gray">
+              Product Thinking, Information Architecture, Interaction, Visual
+              design, Prototyping & testing
+            </p>
+          </div>
+          <div class="mt-4">
+            <p class="px-2 py-4 text-sm bg-gray text-dark-gray">
+              Team: 3 designers, 4 Product Managers
+            </p>
+            <p class="px-2 py-4 text-sm bg-white text-dark-gray">
+              Duration: 3months
+            </p>
+            <p class="px-2 py-4 text-sm bg-gray text-dark-gray">
+              Client : Vberizon
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
   </main>
 </template>
 
