064f3fe849f58944db9e742b4a1af93b7c3d0c8e
Gagandeep Singh <gagansday@gmail.com>
2020-12-30 19:13:51 +0530

    the problem section #8

--- pages/case-study/content-marketing.vue
+++ pages/case-study/content-marketing.vue
@@ -49,15 +49,48 @@
               design, Prototyping & testing
             </p>
           </div>
-          <div class="mt-4">
-            <p class="px-2 py-4 text-sm bg-gray text-dark-gray">
+          <div class="mt-4 text-sm text-dark-gray">
+            <p class="px-2 py-4 bg-gray">
               Team: 3 designers, 4 Product Managers
             </p>
-            <p class="px-2 py-4 text-sm bg-white text-dark-gray">
-              Duration: 3months
+            <p class="px-2 py-4 bg-white">Duration: 3months</p>
+            <p class="px-2 py-4 bg-gray">Client : Vberizon</p>
+          </div>
+        </div>
+      </section>
+      <section class="py-8 mt-8 text-white bg-dark-gray md:py-16">
+        <div class="container mx-auto">
+          <div class="mx-auto md:w-10/12">
+            <h2 class="text-5xl font-bold leading-tight md:leading-none">
+              The Problem
+            </h2>
+            <p class="mt-8">
+              My most recent work with Emerhub where I designed for some of its
+              core offerings like Incorporation, Payroll, Registry with a global
+              reach that helped integrate all their needs under one roof. Having
+              led the module phase for one years in this young,
             </p>
-            <p class="px-2 py-4 text-sm bg-gray text-dark-gray">
-              Client : Vberizon
+            <p class="mt-8">
+              rapidly growing enterprise startup environment - taught me how to
+              balance business goals and engineering constraints as I
+              unrelentingly advocated for the user..
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="py-8 mt-8 md:py-16">
+        <div class="container mx-auto">
+          <div class="mx-auto md:w-10/12">
+            <h2 class="text-5xl font-bold leading-tight md:leading-none">
+              Jumpstarting Research
+            </h2>
+            <p class="mt-8">
+              My most recent work with
+              <a href="#" class="font-bold text-pink">Emerhub</a> where I
+              designed for some of its core offerings like Incorporation,
+              Payroll, Registry with a global reach that helped integrate all
+              their needs under one roof. Having led the module phase for one
+              years in this young,
             </p>
           </div>
         </div>
