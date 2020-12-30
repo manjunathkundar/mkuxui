5ccf0cb0102e364bfc8922a2017545a1b68e5261
Gagandeep Singh <gagansday@gmail.com>
2020-12-30 17:37:05 +0530

    about hero section #7

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="container mx-auto">About</div>
+  <div class="w-full md:container md:w-auto md:mx-auto">
+    <img
+      src="/about/hero.png"
+      alt="Hero Image "
+      class="h-full mx-auto overflow-hidden"
+    />
+  </div>
 </template>
 
 <script>
